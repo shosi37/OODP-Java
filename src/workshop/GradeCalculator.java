15525d7fefb75d3d86f9e5289b2d6e8b333979dc
shoaib siddiqui <shoaibsiddiqui4884@gmail.com>
2025-01-14 23:06:39 +0545

    Added getter method to student class

--- src/workshop/GradeCalculator.java
+++ src/workshop/GradeCalculator.java
@@ -1,5 +1,11 @@
 package workshop;
 
 public class GradeCalculator {
-
+    public static double calculateAverage(int[] grades) {
+        int sum = 0;
+        for (int grade : grades) {
+            sum += grade;
+        }
+        return grades.length > 0 ? (double) sum / grades.length : 0;
+    }
 }
